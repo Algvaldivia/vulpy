53803a4a42eaa169d90f2b63183f654701335b5a
Alexander Lázaro Gomez Valdivia <algvaldivia@gmail.com>
2024-11-17 14:08:43 -0300

    Update other-with-hardcoded.py

--- other-with-hardcoded.py
+++ other-with-hardcoded.py
@@ -1,3 +1,3 @@
 # AzureDevops pre-invalidated PAT token
 # https://resources.github.com/learn/pathways/security/intermediate/customizing-secret-scanning-scope/
-az_pat_token = 'pfnm5xd56muu7srpsme5jxeiwo23vuyavuyo2msziwy34ca7oaha'
+az_pat_token = 'v0bb99zz2f0k3bdp93n6wf1wn63sqbaqlc8v34y1ydqs'
